0918a94994cf90f5de17f509257b6833b4053e90
imanavyblue <134798126+imanavyblue@users.noreply.github.com>
2024-09-20 10:04:49 +0700

    Update upload_model.py

--- upload_model.py
+++ upload_model.py
@@ -1,21 +1,24 @@
 from huggingface_hub import HfApi
 import os
 
-# ตรวจสอบว่ามี token หรือไม่
 HF_API_TOKEN = os.getenv("HF_API_TOKEN")
 
 if HF_API_TOKEN:
-    # สร้างอ็อบเจกต์ HfApi
     api = HfApi()
+    
+    # ตรวจสอบให้แน่ใจว่าไฟล์มีอยู่ในระบบ
+    model_path = "models/Inception_V3.h5"  # ตัวอย่าง: เส้นทางไปยังไฟล์โมเดล
 
-    # อัปโหลดไฟล์โมเดลไปยัง Hugging Face
-    api.upload_file(
-        path_or_fileobj="Inception_V3.h5",  # ไฟล์โมเดลในเครื่อง
-        path_in_repo="Inception_V3.h5",      # ชื่อไฟล์ที่จะบันทึกใน Hugging Face repository
-        repo_id="Suphawan/Melanoma-3",       # Repository ของคุณ
-        repo_type="model",                   # ประเภทของ repository
-        token=HF_API_TOKEN                   # ใช้ API token สำหรับการยืนยันตัวตน
-    )
-    print("อัปโหลดโมเดลเสร็จสมบูรณ์")
+    if os.path.exists(model_path):
+        api.upload_file(
+            path_or_fileobj=model_path,
+            path_in_repo="Inception_V3.h5",
+            repo_id="Suphawan/Melanoma-3",
+            repo_type="model",
+            token=HF_API_TOKEN
+        )
+        print("อัปโหลดโมเดลสำเร็จ")
+    else:
+        print(f"ไฟล์ {model_path} ไม่พบในระบบ")
 else:
     print("ไม่พบ HF_API_TOKEN. กรุณาตรวจสอบการตั้งค่า API token.")
